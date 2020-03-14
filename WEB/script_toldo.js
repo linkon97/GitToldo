a23103d61159973c8ea4db4d13fce3f442b196da
Jaime <jaimevalera97@gmail.com>
2020-03-14 02:30:32 +0100

    first

---
+++ WEB/script_toldo.js
@@ -0,0 +1,25 @@
+
+ 
+function showDiv() {  
+
+  if (manifesto.style.display === "block") { 
+    manifesto.style.display = "none";
+    var x = document.getElementsByClassName("header_bar");
+    x[0].style.height= "83%";
+   titulo.style.display="none";
+  } else {
+    manifesto.style.display = "block";
+    var x = document.getElementsByClassName("header_bar");
+  	x[0].style.height= "28%";
+  	titulo.style.display="block";
+
+  }
+}
+
+
+ 
+
+
+
+
+  	
