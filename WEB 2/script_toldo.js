1efedfd15314d2c4167b0dd1df95b8cc236eac6a
linkon97 <jaimevalera97@gmail.com>
2020-03-16 22:35:59 +0100

    16/03/2020
    pre barra de menu

--- WEB 2/script_toldo.js
+++ WEB 2/script_toldo.js
@@ -1,28 +1,28 @@
 
- 
-function showDiv() {  
-  if (manifesto.style.display === "block") { 
+
+function showDiv() {
+  if (manifesto.style.display === "block") {
     manifesto.style.display = "none";
     var x = document.getElementsByClassName("header_bar");
-    x[0].style.height= "83%";
+    x[0].style.height= "100%";
    titulo.style.display="none";
+   var check =0;
   } else {
     manifesto.style.display = "block";
     var x = document.getElementsByClassName("header_bar");
-  	x[0].style.height= "28%";
+  	x[0].style.height= "29%";
   	titulo.style.display="block";
+    var check =1;
 
   }
 
 }
 function showMenu(){
-  
-}
-
-
- 
-
-
-
-
-  	
+    var y = document.getElementsByClassName("menu_div");
+    if (y[0].style.display==="block") {
+      y[0].style.display="none";
+    }else {
+      y[0].style.display="block";
+    }
+    
+    }
